02dd9a722d10bebb83b995282db277e7ce9e2bda
Zhu Mei <meizhu812@outlook.com>
2020-11-05 15:56:26 +0800

    [Zhu Mei]-add error handling for invalid fields.

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/controller/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/controller/GlobalExceptionHandler.java
@@ -2,11 +2,15 @@ package com.thoughtworks.capacity.gtb.mvc.controller;
 
 import com.thoughtworks.capacity.gtb.mvc.controller.dto.Error;
 import com.thoughtworks.capacity.gtb.mvc.exception.UsernameExistsException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,5 +19,15 @@ public class GlobalExceptionHandler {
     @ResponseBody
     public Error handleUserExists(UsernameExistsException exception) {
         return new Error(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public Error handleUserExists(MethodArgumentNotValidException exception) {
+        String errorMessages = exception.getBindingResult().getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining("|"));
+        return new Error(HttpStatus.BAD_REQUEST.value(), errorMessages);
     }
 }
