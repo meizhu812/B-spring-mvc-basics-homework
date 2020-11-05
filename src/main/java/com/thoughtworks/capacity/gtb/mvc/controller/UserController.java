014b6af4e618d0f340c1502d6f516998fee6d29e
Zhu Mei <meizhu812@outlook.com>
2020-11-05 17:27:30 +0800

    [Zhu Mei]-refactor controller by using ResponseEntity

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/controller/UserController.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/controller/UserController.java
@@ -3,12 +3,13 @@ package com.thoughtworks.capacity.gtb.mvc.controller;
 import com.thoughtworks.capacity.gtb.mvc.controller.dto.RegisterRequest;
 import com.thoughtworks.capacity.gtb.mvc.model.User;
 import com.thoughtworks.capacity.gtb.mvc.service.UserService;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
+import java.net.URI;
 
 @RestController
 @Validated
@@ -20,15 +21,16 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void register(@RequestBody @Valid RegisterRequest registerRequest) {
+    public ResponseEntity<Void> register(@RequestBody @Valid RegisterRequest registerRequest) {
         userService.register(registerRequest);
+        return ResponseEntity.created(URI.create("UNKNOWN"))
+                .build();  //TODO 需要其他endpoint直达用户信息
     }
 
     @GetMapping("/login")
-    @ResponseStatus(HttpStatus.OK)
-    public User login(@RequestParam @Pattern(message = "用户名不合法", regexp = "^[\\w]{3,10}$") String username,
-                      @RequestParam @Pattern(message = "密码不合法", regexp = "^.{5,12}$") String password) {
-        return userService.login(username, password);
+    public ResponseEntity<User> login(@RequestParam @Pattern(message = "用户名不合法", regexp = "^[\\w]{3,10}$") String username,
+                                      @RequestParam @Pattern(message = "密码不合法", regexp = "^.{5,12}$") String password) {
+        return ResponseEntity.ok()
+                .body(userService.login(username, password));
     }
 }
